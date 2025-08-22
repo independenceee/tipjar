02e02f4f898b9844eb827c89066b7da1d757f0d8
Nguyen Duy Khanh <nguyenkhanh17112003@gmail.com>
2025-08-22 21:17:19 +0700

    feat: decommit successfully setting hydra hub and fix hydra connection

--- tests/tipjar.test.ts
+++ tests/tipjar.test.ts
@@ -10,7 +10,7 @@ describe("Hydra Tipjar Cardano", function () {
 
   beforeEach(function () {
     hydraProvider = new HydraProvider({
-      httpUrl: "https://194.195.87.66:4001",
+      httpUrl: "http://194.195.87.66:4001",
       wsUrl: "ws://194.195.87.66:4001",
     });
     blockfrostProvider = new BlockfrostProvider("previewkiE6gOIulzmz9HIxyrTW2axrxWj4wMHt");
@@ -35,44 +35,23 @@ describe("Hydra Tipjar Cardano", function () {
   jest.setTimeout(600000000);
 
   it("should make and submit a valid tx", async () => {
-    await hydraProvider.init();
     await hydraProvider.connect();
 
-    const pp = await hydraProvider.fetchProtocolParameters();
-    console.log(pp);
+    await hydraProvider.init();
+    const utxosAlice = (await meshWallet.getUtxos())[12];
+    console.log(await meshWallet.getChangeAddress());
+
+    const protocolParameters = await hydraProvider.fetchProtocolParameters();
+
     const utxos = await hydraProvider.fetchUTxOs();
+    console.log("UTxO", utxos);
 
-    // meshTxBuilder = new MeshTxBuilder({
-    //   fetcher: blockfrostProvider,
-    //   submitter: blockfrostProvider,
-    //   params: pp,
-    //   isHydra: true,
-    // });
-    // const unsignedTx = await meshTxBuilder
-    //   .txIn("cd8d9b66df467df82cf6df10a0dbd24847a27ac280e1033e509a8bc0de8d2579", 2)
-    //   .txOut("", [
-    //     {
-    //       unit: "lovelace",
-    //       quantity: "30000000",
-    //     },
-    //   ])
-    //   .selectUtxosFrom(utxos)
-    //   .setFee("0")
-    //   .changeAddress("")
-    //   .setNetwork("preprod")
-    //   .complete();
+    const commitTx = await hydraInstance.commitFunds("008a70e126e107cca6f272d78bfb40ce9f78ae7bb1b6d6dc4c382c0f9c5b72e9", 0);
 
-    // console.log(unsignedTx);
-
-    // const tx = await hInstance.commitBlueprint("ad16a3a415763e8662469c868038a659f5076a174633323894666f4c9d2e60d1", 1, {
-    //   cborHex: unsignedTx,
-    //   description: "a new blueprint tx",
-    //   type: "Tx ConwayEra",
-    // });
-    // console.log("tx", tx);
-    // const signedTx = await wallet.signTx(tx, true);
-    // const txHash = await wallet.submitTx(signedTx);
-    // console.log("commit txhash:", txHash);
-    // expect(typeof txHash).toBe("string");
+    console.log("tx", commitTx);
+    const signedTx = await meshWallet.signTx(commitTx, true);
+    console.log(signedTx);
+    const commitTxHash = await meshWallet.submitTx(signedTx);
+    console.log("txHash: ", commitTxHash);
   });
 });
