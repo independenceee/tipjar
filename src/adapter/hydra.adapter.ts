26a197b8e39e348f04ca591a0fd82f2e21e93437
Nguyen Duy Khanh <nguyenkhanh17112003@gmail.com>
2025-09-03 10:43:15 +0700

    feat: common and basic state management in head hydra.

--- src/adapter/hydra.adapter.ts
+++ src/adapter/hydra.adapter.ts
@@ -1,6 +1,5 @@
 import { IFetcher, MeshTxBuilder, MeshWallet, UTxO } from "@meshsdk/core";
 import { HydraInstance, HydraProvider } from "@meshsdk/hydra";
-import { HYDRA_HTTP_URL } from "~/constants/enviroments";
 import { blockfrostProvider } from "~/providers/cardano";
 
 export class HydraAdapter {
@@ -10,47 +9,28 @@ export class HydraAdapter {
     protected hydraInstance!: HydraInstance;
     protected hydraProvider: HydraProvider;
 
-    constructor({ meshWallet = null! }: { meshWallet: MeshWallet }) {
+    constructor({ meshWallet = null!, hydraProvider = null! }: { meshWallet: MeshWallet; hydraProvider: HydraProvider }) {
         this.meshWallet = meshWallet;
         this.fetcher = blockfrostProvider;
-
-        this.hydraProvider = new HydraProvider({
-            httpUrl: HYDRA_HTTP_URL,
-        });
-
+        this.hydraProvider = hydraProvider;
         this.hydraInstance = new HydraInstance({
             submitter: blockfrostProvider,
             provider: this.hydraProvider,
             fetcher: blockfrostProvider,
         });
 
-        this.initialize();
+        this.initialized();
     }
 
-    protected initialize = async () => {
-        const protocolParams = await this.hydraProvider.fetchProtocolParameters();
+    protected initialized = async () => {
+        const protocolParameters = await this.hydraProvider.fetchProtocolParameters();
         this.meshTxBuilder = new MeshTxBuilder({
-            fetcher: this.fetcher,
-            submitter: this.hydraProvider !== null ? this.hydraProvider : blockfrostProvider,
-            params: protocolParams,
+            params: protocolParameters,
+            fetcher: this.hydraProvider,
+            submitter: this.hydraProvider,
             isHydra: !!this.hydraProvider,
         });
     };
 
-    protected getWalletForTx = async (): Promise<{
-        utxos: UTxO[];
-        collateral: UTxO;
-        walletAddress: string;
-    }> => {
-        const utxos = await this.meshWallet.getUtxos();
-        const collaterals = await this.meshWallet.getCollateral();
-        const walletAddress = await this.meshWallet.getChangeAddress();
-        if (!utxos || utxos.length === 0) throw new Error("No UTXOs found in getWalletForTx method.");
-
-        if (!collaterals || collaterals.length === 0) throw new Error("No collateral found in getWalletForTx method.");
-
-        if (!walletAddress) throw new Error("No wallet address found in getWalletForTx method.");
-
-        return { utxos, collateral: collaterals[0], walletAddress };
-    };
+   
 }
--- src/services/hydra.service.ts
+++ src/services/hydra.service.ts
@@ -1,12 +1,13 @@
 "use server";
 
 import { MeshWallet } from "@meshsdk/core";
+import { HydraProvider } from "@meshsdk/hydra";
 import { isNil } from "lodash";
-import { APP_NETWORK_ID } from "~/constants/enviroments";
+import { APP_NETWORK_ID, HYDRA_HTTP_URL, HYDRA_WS_URL } from "~/constants/enviroments";
 import { blockfrostProvider } from "~/providers/cardano";
 import { HydraTxbuilder } from "~/txbuilders/hydra.txbuilder";
 
-export const commit = async function ({ walletAddress }: { walletAddress: string }) {
+export const commit = async function ({ walletAddress, isCreator = false }: { walletAddress: string; isCreator: boolean }) {
     try {
         if (isNil(walletAddress)) {
             throw new Error("walletAddress has been required.");
@@ -22,7 +23,12 @@ export const commit = async function ({ walletAddress }: { walletAddress: string
             },
         });
 
-        const hydraTxbuilder: HydraTxbuilder = new HydraTxbuilder({ meshWallet: meshWallet });
+        const hydraProvider = new HydraProvider({
+            httpUrl: isCreator ? HYDRA_HTTP_URL : "",
+            wsUrl: isCreator ? HYDRA_WS_URL : "",
+        });
+
+        const hydraTxbuilder: HydraTxbuilder = new HydraTxbuilder({ meshWallet: meshWallet, hydraProvider: hydraProvider });
         const unsignedTx = await hydraTxbuilder.commit();
 
         return unsignedTx;
